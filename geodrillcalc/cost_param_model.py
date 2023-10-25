9e28404a27f53856963a6c1582f4a1376a514fdb
Doran Huh <08dhuh@gmail.com>
2023-10-25 20:22:19 +1100

    Finished stage 1.defining parameters for screened interval

---
+++ geodrillcalc/cost_param_model.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+
+@dataclass
+class MaterialCost:
+    name:str
+    cost:float
+    margin_percentage:float
+    margins: tuple
+
+    def update(self, cost=None, margin_percentage=None, margins=None):
+        """
+        Update the cost of the material
+
+        """
+        return None
