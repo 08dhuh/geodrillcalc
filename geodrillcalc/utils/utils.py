cea4684d38e84be085894ef93696ca7d297d5e5a
Doran Huh <08dhuh@gmail.com>
2023-11-03 08:31:26 +1100

    added initialisation methods

--- geodrillcalc/utils/utils.py
+++ geodrillcalc/utils/utils.py
@@ -3,19 +3,21 @@ import logging
 import numpy as np
 import functools
 
+
 def getlogger() -> logging.Logger:
     """
     Get the pre-configured logger instance.
 
-    
+
     """
-    logging.basicConfig(level= logging.INFO, format="%(asctime)s %(levelname)s %(message)s", datefmt="%H:%M:%S %d-%m-%Y")
+    logging.basicConfig(
+        level=logging.INFO, format="%(asctime)s %(levelname)s %(message)s", datefmt="%H:%M:%S %d-%m-%Y")
     logger = logging.getLogger()
     return logger
 
 
-def find_nearest_value(val, 
-                       array, 
+def find_nearest_value(val,
+                       array,
                        larger_than_or_equal_val=True,
                        one_size_larger=False):
     """
@@ -35,10 +37,11 @@ def find_nearest_value(val,
         (float) The nearest value in the 'array' relative to 'val'. If 'one_size_larger' is True,
         and a larger or equal value is not found, it returns the next nominal casing size larger.
     """
-    #casting applied
+    # casting applied
     if larger_than_or_equal_val:
-        valid_vals = array[array > val] if one_size_larger else array[array >= val]
-    else: 
+        valid_vals = array[array >
+                           val] if one_size_larger else array[array >= val]
+    else:
         valid_vals = array
 
     nearest_idx = np.argmin(np.abs(valid_vals - val))
@@ -59,14 +62,14 @@ def find_next_largest_value(val, array):
     Returns:
         (float) The next nominal casing size larger than 'val' in the 'array'.
     """
-    return find_nearest_value(val, 
+    return find_nearest_value(val,
                               array,
                               one_size_larger=True)
 
+    # TODO: read casing diameter csv, import the column in meters,
+    # use round_algorithm function predefined
 
-    #TODO: read casing diameter csv, import the column in meters,
-    #use round_algorithm function predefined 
-    
+
 def validate_data(func):
     @functools.wraps(func)
     def wrapper(self, *args, **kwargs):
@@ -82,3 +85,23 @@ def validate_data(func):
 
     return wrapper
 
+
+def validate(value, condition=None):
+    if condition:
+        return condition(value)
+    return True
+
+
+@classmethod
+def get_all_non_boilerplate_attributes(cls):
+    """Returns a list of all non-boilerplate attributes of the class.
+
+    Returns:
+        A list of strings, where each string is the name of a non-boilerplate
+        attribute.
+    """
+
+    boilerplate_attribute_names = ["__class__", "__dict__", "__module__"]
+    attribute_names = [attr for attr in dir(cls) if not attr.startswith(
+        "__") and attr not in boilerplate_attribute_names]
+    return attribute_names
