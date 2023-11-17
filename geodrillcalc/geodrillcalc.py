c739c54cf9c93104498259fc346047eb20496428
Doran Huh <08dhuh@gmail.com>
2023-11-17 12:53:59 +1100

    more pylinting and added logger method

--- geodrillcalc/geodrillcalc.py
+++ geodrillcalc/geodrillcalc.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python
+"""
+This module defines a class, GeoDrillCalcInterface, that serves as an interface
+for calculating wellbore parameters. It utilizes the WellBoreDict class for managing
+wellbore data and the CalcPipeline class for performing wellbore calculations.
+
+Example Usage:
+    geo_interface = GeoDrillCalcInterface()
+
+    result_wbd = geo_interface.calculate_and_return_wellbore_parameters(
+        is_production_pump=True,
+        depth_data=depth_data,
+        initial_input_data=initial_values
+    )
+
+Note: Ensure that you provide valid depth data and initial input data
+when using the 'calculate_and_return_wellbore_parameters' method.
+"""
 from .wellbore_dict import WellBoreDict
 from .calc_pipeline import CalcPipeline
 from .utils.utils import getlogger
@@ -11,12 +28,16 @@ class GeoDrillCalcInterface:
     Attributes:
     - wbd: An instance of the WellBoreDict class for managing wellbore data.
     - cpl: An instance of the CalcPipeline class for performing wellbore calculations.
-    - is_production_pump: A boolean indicating whether the pump used is for production (True) or injection (False).
+    - is_production_pump: A boolean indicating whether the pump used is for production or injection.
     - logger: A logger for handling log messages in the GeoDrillCalcInterface class.
 
     Methods:
-    - calculate_and_return_wellbore_parameters(self, is_production_pump: bool, depth_data, initial_input_data):
-      Calculates wellbore parameters using WellBoreDict and CalcPipeline and returns the WellBoreDict instance.
+    - calculate_and_return_wellbore_parameters:
+      Calculates wellbore parameters using WellBoreDict and CalcPipeline 
+      and returns the WellBoreDict instance.
+
+    - set_loglevel:
+      Sets the logging level of the current instance's logger.
 
     Example Usage:
 
@@ -42,6 +63,67 @@ class GeoDrillCalcInterface:
                                                  is_production_pump: bool,
                                                  depth_data,
                                                  initial_input_data):
+        """
+        Orchestration method for inputting and calculating 
+        the model parameters
+
+        Parameters:
+            is_production_pump: bool
+            depth_data: pd.DataFrame
+            initial_input_data: dict
+
+        Returns:
+            WellBoreDict instance containing the results
+
+        Note:
+            Make sure the inputs follow the format below:
+                depth_data = {
+                    "aquifer_layer": [
+                        "QA_UTQA",
+                        "UTQD",
+                        "UTAF",
+                        "UTD",
+                        "UMTA",
+                        "UMTD",
+                        "LMTA",
+                        "LTA",
+                        "BSE"
+                    ],
+                    "is_aquifer": [
+                        True,
+                        False,
+                        True,
+                        False,
+                        True,
+                        False,
+                        True,
+                        True,
+                        False
+                    ],
+                    "depth_to_base": [
+                        3,
+                        53,
+                        112,
+                        150,
+                        150,
+                        1000,
+                        1000,
+                        1221,
+                        1421
+                    ]
+                }
+
+                initial_values = {
+                    "required_flow_rate": 4320,
+                    "hydraulic_conductivity": 5,
+                    "average_porosity": 0.25,
+                    "bore_lifetime_year": 30,
+                    "groundwater_depth": 25,
+                    "long_term_decline_rate": 1,
+                    "allowable_drawdown": 25,
+                    "safety_margin": 25
+                }
+        """
         self._initialise(depth_data, initial_input_data)
         self.cpl = CalcPipeline(self.wbd)
         self.cpl.calc_pipeline(is_production_pump)
@@ -49,68 +131,26 @@ class GeoDrillCalcInterface:
         return self.wbd
 
     def _initialise(self, depth_data, initial_input_data):
+        """
+        Initialises and prepares the instance for the pipeline
+        """
         if self.wbd is None:
             self.wbd = WellBoreDict()
         self.wbd.initialise_and_validate_input_data(depth_data=depth_data,
                                                     **initial_input_data)
 
     def _log_outcome(self):
+        """
+        Logs the outcome of the pipeline at INFO level
+        """
         if not self.wbd.is_initialised or not self.wbd.calculation_completed:
             return
         for key in self.wbd.outcome_params:
             value = getattr(self.wbd, key)
             self.logger.info(f"{key}: {value}")
 
-
-# if __name__ == "__main__":
-#     depth_data = {
-#         "aquifer_layer": [
-#             "QA_UTQA",
-#             "UTQD",
-#             "UTAF",
-#             "UTD",
-#             "UMTA",
-#             "UMTD",
-#             "LMTA",
-#             "LTA",
-#             "BSE"
-#         ],
-#         "is_aquifer": [
-#             True,
-#             False,
-#             True,
-#             False,
-#             True,
-#             False,
-#             True,
-#             True,
-#             False
-#         ],
-#         "depth_to_base": [
-#             3,
-#             53,
-#             112,
-#             150,
-#             150,
-#             1000,
-#             1000,
-#             1221,
-#             1421
-#         ]
-#     }
-
-#     initial_values = {
-#         "required_flow_rate": 4320,
-#         "hydraulic_conductivity": 5,
-#         "average_porosity": 0.25,
-#         "bore_lifetime_year": 30,
-#         "groundwater_depth": 25,
-#         "long_term_decline_rate": 1,
-#         "allowable_drawdown": 25,
-#         "safety_margin": 25
-#     }
-#     gdc = GeoDrillCalcInterface()
-#     print("loading success")
-#     gdc.calculate_and_return_wellbore_parameters(True,
-#                                                  depth_data,
-#                                                  initial_values)
+    def set_loglevel(self, loglevel: int or str):
+        """
+        Sets the logging level of current instance's logger.
+        """
+        self.logger.setLevel(loglevel)
