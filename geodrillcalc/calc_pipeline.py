d43a43e45149944e452dff46d984d1fa74632079
Doran Huh <08dhuh@gmail.com>
2024-07-25 15:32:44 +1000

    upgraded package version

--- geodrillcalc/calc_pipeline.py
+++ geodrillcalc/calc_pipeline.py
@@ -29,11 +29,60 @@ class CalcPipeline:
         Requires a boolean argument (True for production pump, False for injection pump).
     4. calc_pipeline: Encapsulates the pipeline methods in a single method
 
+    Required Parameters:
+        depth_data = {
+            "aquifer_layer": [
+                '100qa',
+                '103utqd',
+                '105utaf',
+                '106utd',
+                '107umta',
+                '108umtd',
+                '109lmta',
+                '111lta',
+                '114bse'
+            ],
+            "is_aquifer": [
+                True,
+                False,
+                True,
+                False,
+                True,
+                False,
+                True,
+                True,
+                False
+            ],
+            "depth_to_base": [
+                3,
+                53,
+                112,
+                150,
+                150,
+                1000,
+                1000,
+                1221,
+                1421
+            ]
+        }
+
+        initial_values = {
+            "required_flow_rate": 4320,
+            "hydraulic_conductivity": 5,
+            "average_porosity": 0.25,
+            "bore_lifetime_year": 30,
+            "groundwater_depth": 25,
+            "long_term_decline_rate": 1,
+            "allowable_drawdown": 25,
+            "safety_margin": 25,
+            "target_aquifer_layer": "109lmta",
+            "top_aquifer_layer": "100qa"
+        }
+
     Usage Example:
     wbd = WellBoreDict()
     wbd.initialise_and_validate_input_data(depth_data=depth_data, **initial_values)  
-    # Refer to WellBoreDict.__doc__ for the required keyword arguments.
-
+    
     calc_injectionpipe = CalcPipeline(wbd)
     calc_injectionpipe.calc_pipeline(is_production_pump=True)
     """
